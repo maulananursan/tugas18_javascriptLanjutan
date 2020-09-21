168722aa9c5a7fd577c12220590a4f4e1577dd0d
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-09-21 11:33:43 +0700

    Add files via upload

---
+++ tugas18-1.js
@@ -0,0 +1,5 @@
+var kalimat = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+
+potong = kalimat.split(" ")
+
+console.log(potong[3]," ",potong[4])
